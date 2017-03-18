9942cdb830660395b98d19732ccb0b797061b876
Mathieu Tauban <mathieu.tauban@gmail.com>
2017-03-18 11:43:20 +0000

    improved install

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 #!flask/bin/python
+from os import getenv
 from app import app
-app.run(host='0.0.0.0',debug=True)
+
+app.run(host=getenv('IP', '0.0.0.0'),port=int(getenv('PORT', '8080')),debug=True)
